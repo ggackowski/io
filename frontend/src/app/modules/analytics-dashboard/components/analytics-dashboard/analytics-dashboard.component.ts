bb9ca3a2ec93dd1ec7a64a3502beac3030e38ce2
Pan-Maciek <koziejka.com@gmail.com>
2021-04-10 15:51:39 +0200

    reading data from the server

--- frontend/src/app/modules/analytics-dashboard/components/analytics-dashboard/analytics-dashboard.component.ts
+++ frontend/src/app/modules/analytics-dashboard/components/analytics-dashboard/analytics-dashboard.component.ts
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./analytics-dashboard.component.scss']
 })
 export class AnalyticsDashboardComponent implements OnInit {
-
+  text: string = "There will be something soon"
   constructor() { }
 
   ngOnInit(): void {
+    fetch('/api').then(res => res.json()).then(data => {
+      this.text = data
+    })
   }
 
 }
