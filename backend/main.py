cd499ca2e07eab06191451e13808eb95d06b563b
Pan-Maciek <koziejka.com@gmail.com>
2021-04-11 20:44:59 +0200

    sending mock data from server

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,10 @@
 from flask import Flask, jsonify
 app = Flask(__name__)
 
-@app.route('/api')
+
+@app.route('/api/data/infections')
 def hello_world():
-    return jsonify('Hello, World!')
+    return jsonify({
+        'dataSets': [{ 'data': [1, 2, 3, 4, 5], 'label': 'test' }],
+        'labels': [ '11.03', '12.03', '13.03', '14.03', '15.03' ]
+    })
--- frontend/src/app/modules/analytics-dashboard/services/analytics-dashboard-rest.service.ts
+++ frontend/src/app/modules/analytics-dashboard/services/analytics-dashboard-rest.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import {Observable, of} from "rxjs";
+import {Observable} from "rxjs";
 import {BarChartData} from "../model/bar-chart-data.model";
 
 @Injectable({
@@ -13,19 +13,6 @@ export class AnalyticsDashboardRestService {
   ) { }
 
   public getTweetsCountChartData(): Observable<BarChartData> {
-    return of(
-      {
-      dataSets: [
-        {
-          data: [1, 2, 3, 4, 5],
-          label: 'test'
-        }
-      ],
-      labels: [
-        'a', 'b', 'c', 'd', 'e'
-      ]
-    }
-    );
-    //return this.httpClient.get('');
+    return this.httpClient.get<BarChartData>('/api/data/infections');
   }
 }
