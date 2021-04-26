66723af25f7a7f5e3fe133a6e1db909ae2bedb1a
Pan-Maciek <koziejka.com@gmail.com>
2021-04-26 22:13:34 +0200

    using real data

--- frontend/src/app/modules/analytics-dashboard/services/analytics-dashboard-rest.service.ts
+++ frontend/src/app/modules/analytics-dashboard/services/analytics-dashboard-rest.service.ts
@@ -27,29 +27,14 @@ export class AnalyticsDashboardRestService {
     return this.httpClient.get<Array<string>>('/api/data/tweets/count', {params: {start:
           startDate.toISOString(),
         end: endDate.toISOString()}}).pipe(tap(console.log));
-
-    return of({
-      date: ['1'],
-      value: [10]
-    }).pipe(delay(200));
   }
 
   public getInfectionsDataInRange(startDate: Date, endDate: Date): Observable<BarChartData> {
-    // console.log(startDate, new Date(2021, 3, 1), startDate === new Date(2021, 3, 1));
-    // if (startDate. new Date(2021, 3, 1))
-      return of({
-        date: ['10', '20'],
-        value: [10, 20]
-      }).pipe(delay(400))
-    // return of({
-    //   date: ['11', '22'],
-    //   value: [13, 24]
-    // }).pipe(delay(400))
-    // return this.httpClient.get<BarChartData>('/api/data/active_cases',
-    //   {params: {start: startDate.toISOString(), end: endDate.toISOString()}})
-    //   .pipe(map(data => {
-    //     data.date = data.date.map(x => dateTimeFormat.format(new Date(x as string)))
-    //     return data
-    // }), tap(console.log));
+    return this.httpClient.get<BarChartData>('/api/data/active_cases',
+      {params: {start: startDate.toISOString(), end: endDate.toISOString()}})
+      .pipe(map(data => {
+        data.date = data.date.map(x => dateTimeFormat.format(new Date(x as string)))
+        return data
+    }), tap(console.log));
   }
 }
