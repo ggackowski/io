aa0d9a66d0caee03e367b7a6778403adac421bf9
Pan-Maciek <koziejka.com@gmail.com>
2021-05-04 10:57:49 +0200

    moved to post, added tags

--- backend/main.py
+++ backend/main.py
@@ -10,10 +10,10 @@ client = MongoClient("mongodb+srv://admin:admin@cluster0.kvxff.mongodb.net/io?re
 db = client.io
 app = Flask(__name__)
 
-@app.route('/api/data/active_cases')
+@app.route('/api/data/active_cases', methods=['POST'])
 def get_infectionis():
-    start = datetime.fromisoformat(request.args.get('start').replace("Z", ""))
-    end = datetime.fromisoformat(request.args.get('end').replace("Z", ""))
+    start = datetime.fromisoformat(request.json['start'].replace("Z", ""))
+    end = datetime.fromisoformat(request.json['end'].replace("Z", ""))
 
     df = pd.DataFrame(db.populationData.find({ "date": { "$gte": start, "$lte": end } }))
 
@@ -22,10 +22,10 @@ def get_infectionis():
         'value': list(df['active_cases'])
     })
 
-@app.route('/api/data/active_cases/today')
+@app.route('/api/data/active_cases/today', methods=['POST'])
 def get_infectionis_today():
-    start = datetime.fromisoformat(request.args.get('start').replace("Z", "")) - timedelta(days=1)
-    end = datetime.fromisoformat(request.args.get('end').replace("Z", ""))
+    start = datetime.fromisoformat(request.json['start'].replace("Z", "")) - timedelta(days=1)
+    end = datetime.fromisoformat(request.json['end'].replace("Z", ""))
 
     df = pd.DataFrame(db.populationData.find({ "date": { "$gte": start, "$lte": end } }))
 
@@ -38,32 +38,50 @@ def get_infectionis_today():
 def get_hashtags():
     return jsonify(hashtags)
 
-@app.route('/api/data/tweets/count')
+@app.route('/api/data/tweets/count', methods=['POST'])
 def get_tweets():
-    start = datetime.fromisoformat(request.args.get('start').replace("Z", ""))
-    end = datetime.fromisoformat(request.args.get('end').replace("Z", ""))
+    start = datetime.fromisoformat(request.json['start'].replace("Z", ""))
+    end = datetime.fromisoformat(request.json['end'].replace("Z", ""))
+    tags = request.json['tags']
 
-    df = pd.DataFrame(db.tweets.aggregate([
-        { "$match" : { "date" : { "$gte": start, "$lte": end } } },
-        { "$group": { "_id": "$date", "count": { "$sum": 1 } } },
-        { "$sort" : { "_id" : 1 } }
-    ]))
+    if tags:
+        df = pd.DataFrame(db.tweets.aggregate([
+            { "$match" : { "date" : { "$gte": start, "$lte": end } } },
+            { "$project": { "tags": {"$size": {"$setIntersection": ["$hashtags", hashtags] }}, "date": True}},
+            { "$group": { "_id": "$date", "count": { "$sum": 1 } } },
+            { "$sort" : { "_id" : 1 } }
+        ]))
+    else:
+        df = pd.DataFrame(db.tweets.aggregate([
+            { "$match" : { "date" : { "$gte": start, "$lte": end } } },
+            { "$group": { "_id": "$date", "count": { "$sum": 1 } } },
+            { "$sort" : { "_id" : 1 } }
+        ]))
 
     return jsonify({
         'date': list(df['_id']),
         'value': list(df['count'])
     })
 
-@app.route('/api/data/tweets/count')
+@app.route('/api/data/tweets/count', methods=['POST'])
 def get_tweets_today():
-    start = datetime.fromisoformat(request.args.get('start').replace("Z", "")) - timedelta(days=1)
-    end = datetime.fromisoformat(request.args.get('end').replace("Z", ""))
+    start = datetime.fromisoformat(request.json['start'].replace("Z", "")) - timedelta(days=1)
+    end = datetime.fromisoformat(request.json['end'].replace("Z", ""))
+    tags = request.json['tags']
 
-    df = pd.DataFrame(db.tweets.aggregate([
-        { "$match" : { "date" : { "$gte": start, "$lte": end } } },
-        { "$group": { "_id": "$date", "count": { "$sum": 1 } } },
-        { "$sort" : { "_id" : 1 } }
-    ]))
+    if tags:
+        df = pd.DataFrame(db.tweets.aggregate([
+            { "$match" : { "date" : { "$gte": start, "$lte": end } } },
+            { "$project": { "tags": {"$size": {"$setIntersection": ["$hashtags", hashtags] }}, "date": True}},
+            { "$group": { "_id": "$date", "count": { "$sum": 1 } } },
+            { "$sort" : { "_id" : 1 } }
+        ]))
+    else:
+        df = pd.DataFrame(db.tweets.aggregate([
+            { "$match" : { "date" : { "$gte": start, "$lte": end } } },
+            { "$group": { "_id": "$date", "count": { "$sum": 1 } } },
+            { "$sort" : { "_id" : 1 } }
+        ]))
 
     return jsonify({
         'date': list(df['_id'])[1:],
