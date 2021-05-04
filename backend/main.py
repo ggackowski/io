37c509b7da13e73455793a73f01b92f57193c9e9
Pan-Maciek <koziejka.com@gmail.com>
2021-05-04 10:30:37 +0200

    fix

--- backend/main.py
+++ backend/main.py
@@ -31,7 +31,7 @@ def get_infectionis_today():
 
     return jsonify({
         'date': list(df['date'])[1:],
-        'value': list(np.array(df['active_cases'])[:-1] - np.array(df['active_cases'])[1:])
+        'value': list(np.array(df['active_cases'])[1:] - np.array(df['active_cases'])[:-1])
     })
 
 @app.route('/api/data/hashtags')
@@ -67,7 +67,7 @@ def get_tweets_today():
 
     return jsonify({
         'date': list(df['_id'])[1:],
-        'value': list(np.array(df['count'])[:-1] - np.array(df['count'])[1:])
+        'value': list(np.array(df['count'])[1:] - np.array(df['count'])[:-1])
     })
 
 hashtags = []
