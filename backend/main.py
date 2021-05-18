5bfa124b881b06a83dbf9519566bc487361c550e
Pan-Maciek <koziejka.com@gmail.com>
2021-05-18 15:26:52 +0200

    applied dry

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,6 @@ from flask import Flask, jsonify, request, make_response
 from pymongo import MongoClient
 from datetime import datetime, timedelta
 import pandas as pd
-from const import *
 import numpy as np
 import json
 
@@ -12,145 +11,55 @@ app = Flask(__name__)
 
 def moving_average(data_set, periods=3):
     weights = np.ones(periods) / periods
-    return np.convolve(data_set, weights, mode='valid')
+    return np.convolve(data_set, weights, mode='same')
+
+def get_simpledata(req, collection, field):
+    start = datetime.fromisoformat(req.json['start'].replace("Z", ""))
+    end = datetime.fromisoformat(req.json['end'].replace("Z", ""))
+    avg = int(req.json.get('avg', 3))
+
+    df = pd.DataFrame(db[collection].find({"date": {"$gte": start, "$lte": end}}))
+    data = df[field]
+
+    return {
+        'date': list(df['date']),
+        'values': [
+            { 'displayName': 'Active cases', 'type': 'bar', 'value': list(data) },
+            { 'displayName': 'Running average', 'type': 'line', 'value': list(moving_average(data, avg)) },
+            { 'displayName': 'Daily difference', 'type': 'bar', 'value': [0] + list(np.array(data)[1:] - np.array(data)[:-1]) },
+        ],
+        'stats': [
+            { 'displayName': 'Mean', 'value': int(np.mean(data)) },
+            { 'displayName': 'Min', 'value': int(np.min(data)) },
+            { 'displayName': 'Max', 'value': int(np.max(data)) },
+            { 'displayName': 'Std', 'value': int(np.std(data)) },
+        ]
+    }
+
 
 @app.route('/api/data/active_cases', methods=['POST'])
 def get_infectionis():
-    start = datetime.fromisoformat(request.json['start'].replace("Z", ""))
-    end = datetime.fromisoformat(request.json['end'].replace("Z", ""))
-    avg = int(request.json.get('avg', 3))
-
-    df = pd.DataFrame(db.populationData.find({ "date": { "$gte": start, "$lte": end } }))
-
-    return jsonify({
-        'date': list(df['date']),
-        'values': [
-            { 'displayName': 'Active cases', 'type': 'bar', 'value': list(df['active_cases']) },
-            { 'displayName': 'Running average', 'type': 'line', 'value': list(moving_average(df['active_cases'], avg)) },
-            { 'displayName': 'Daily difference', 'type': 'bar', 'value': [0] + list(np.array(df['active_cases'])[1:] - np.array(df['active_cases'])[:-1]) },
-        ],
-        'stats': [
-            { 'displayName': 'Mean', 'value': int(np.mean(df['active_cases'])) },
-            { 'displayName': 'Min', 'value': int(np.min(df['active_cases'])) },
-            { 'displayName': 'Max', 'value': int(np.max(df['active_cases'])) },
-            { 'displayName': 'Std', 'value': int(np.std(df['active_cases'])) },
-        ]
-    })
+    return jsonify(get_simpledata(request, 'populationData', 'active_cases'))
 
 @app.route('/api/data/deaths', methods=['POST'])
 def get_deaths():
-    start = datetime.fromisoformat(request.json['start'].replace("Z", ""))
-    end = datetime.fromisoformat(request.json['end'].replace("Z", ""))
-    avg = int(request.json.get('avg', 3))
-
-    df = pd.DataFrame(db.populationData.find({ "date": { "$gte": start, "$lte": end } }))
-
-    return jsonify({
-        'date': list(df['date']),
-        'values': [
-            { 'displayName': 'Deaths', 'type': 'bar', 'value': list(df['deaths']) },
-            { 'displayName': 'Running average', 'type': 'line', 'value': list(moving_average(df['deaths'], avg)) },
-            { 'displayName': 'Daily difference', 'type': 'bar', 'value': [0] + list(np.array(df['deaths'])[1:] - np.array(df['deaths'])[:-1]) },
-        ],
-        'stats': [
-            { 'displayName': 'Mean', 'value': int(np.mean(df['deaths'])) },
-            { 'displayName': 'Min', 'value': int(np.min(df['deaths'])) },
-            { 'displayName': 'Max', 'value': int(np.max(df['deaths'])) },
-            { 'displayName': 'Std', 'value': int(np.std(df['deaths'])) },
-        ]
-    })
+    return jsonify(get_simpledata(request, 'populationData', 'deaths'))
 
 @app.route('/api/data/quarantine', methods=['POST'])
 def get_quarantine():
-    start = datetime.fromisoformat(request.json['start'].replace("Z", ""))
-    end = datetime.fromisoformat(request.json['end'].replace("Z", ""))
-    avg = int(request.json.get('avg', 3))
-
-    df = pd.DataFrame(db.dataSource_hospitalizacja.find({ "date": { "$gte": start, "$lte": end } }))
-
-    return jsonify({
-        'date': list(df['date']),
-        'values': [
-            { 'displayName': 'Quarantained', 'type': 'bar', 'value': list(df['quarantained']) },
-            { 'displayName': 'Running average', 'type': 'line', 'value': list(moving_average(df['quarantained'], avg)) },
-            { 'displayName': 'Daily difference', 'type': 'bar', 'value': [0] + list(np.array(df['quarantained'])[1:] - np.array(df['quarantained'])[:-1]) },
-        ],
-        'stats': [
-            { 'displayName': 'Mean', 'value': int(np.mean(df['quarantained'])) },
-            { 'displayName': 'Min', 'value': int(np.min(df['quarantained'])) },
-            { 'displayName': 'Max', 'value': int(np.max(df['quarantained'])) },
-            { 'displayName': 'Std', 'value': int(np.std(df['quarantained'])) },
-        ]
-    })
+    return jsonify(get_simpledata(request, 'dataSource_hospitalizacja', 'quarantained'))
 
 @app.route('/api/data/intense', methods=['POST'])
 def get_intense():
-    start = datetime.fromisoformat(request.json['start'].replace("Z", ""))
-    end = datetime.fromisoformat(request.json['end'].replace("Z", ""))
-    avg = int(request.json.get('avg', 3))
-
-    df = pd.DataFrame(db.respiratoryData.find({ "date": { "$gte": start, "$lte": end } }))
-
-    return jsonify({
-        'date': list(df['date']),
-        'values': [
-            { 'displayName': 'Intense therapy', 'type': 'bar', 'value': list(df['used_life_saving_kit']) },
-            { 'displayName': 'Running average', 'type': 'line', 'value': list(moving_average(df['used_life_saving_kit'], avg)) },
-            { 'displayName': 'Daily difference', 'type': 'bar', 'value': [0] + list(np.array(df['used_life_saving_kit'])[1:] - np.array(df['used_life_saving_kit'])[:-1]) },
-        ],
-        'stats': [
-            { 'displayName': 'Mean', 'value': int(np.mean(df['used_life_saving_kit'])) },
-            { 'displayName': 'Min', 'value': int(np.min(df['used_life_saving_kit'])) },
-            { 'displayName': 'Max', 'value': int(np.max(df['used_life_saving_kit'])) },
-            { 'displayName': 'Std', 'value': int(np.std(df['used_life_saving_kit'])) },
-        ]
-    })
+    return jsonify(get_simpledata(request, 'respiratoryData', 'used_life_saving_kit'))
 
 @app.route('/api/data/vaccinated', methods=['POST'])
 def get_vaccinated():
-    start = datetime.fromisoformat(request.json['start'].replace("Z", ""))
-    end = datetime.fromisoformat(request.json['end'].replace("Z", ""))
-    avg = int(request.json.get('avg', 3))
-
-    df = pd.DataFrame(db.dataSource_szczepienia.find({ "date": { "$gte": start, "$lte": end } }))
-
-    return jsonify({
-        'date': list(df['date']),
-        'values': [
-            { 'displayName': 'Vaccinated', 'type': 'bar', 'value': list(df['vaccinated']) },
-            { 'displayName': 'Running average', 'type': 'line', 'value': list(moving_average(df['vaccinated'], avg)) },
-            { 'displayName': 'Daily difference', 'type': 'bar', 'value': [0] + list(np.array(df['vaccinated'])[1:] - np.array(df['vaccinated'])[:-1]) },
-        ],
-        'stats': [
-            { 'displayName': 'Mean', 'value': int(np.mean(df['vaccinated'])) },
-            { 'displayName': 'Min', 'value': int(np.min(df['vaccinated'])) },
-            { 'displayName': 'Max', 'value': int(np.max(df['vaccinated'])) },
-            { 'displayName': 'Std', 'value': int(np.std(df['vaccinated'])) },
-        ]
-    })
+    return jsonify(get_simpledata(request, 'dataSource_szczepienia.', 'vaccinated'))
 
 @app.route('/api/data/cured', methods=['POST'])
 def get_cured():
-    start = datetime.fromisoformat(request.json['start'].replace("Z", ""))
-    end = datetime.fromisoformat(request.json['end'].replace("Z", ""))
-    avg = int(request.json.get('avg', 3))
-
-    df = pd.DataFrame(db.populationData.find({ "date": { "$gte": start, "$lte": end } }))
-
-    return jsonify({
-        'date': list(df['date']),
-        'values': [
-            { 'displayName': 'Cured', 'type': 'bar', 'value': list(df['cured']) },
-            { 'displayName': 'Running average', 'type': 'line', 'value': list(moving_average(df['cured'], avg)) },
-            { 'displayName': 'Daily difference', 'type': 'bar', 'value': [0] + list(np.array(df['cured'])[1:] - np.array(df['cured'])[:-1]) },
-        ],
-        'stats': [
-            { 'displayName': 'Mean', 'value': int(np.mean(df['cured'])) },
-            { 'displayName': 'Min', 'value': int(np.min(df['cured'])) },
-            { 'displayName': 'Max', 'value': int(np.max(df['cured'])) },
-            { 'displayName': 'Std', 'value': int(np.std(df['cured'])) },
-        ]
-    })
+    return jsonify(get_simpledata(request, 'populationData.', 'cured'))
 
 @app.route('/api/data/hashtags')
 def get_hashtags():
